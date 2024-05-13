a84ed0b2a94148d82ac514a7636bbe1430043148
Maksim Litvinov <metalim@gmail.com>
2024-05-13 22:01:35 +0300

    no walls

--- snake2.py
+++ snake2.py
@@ -86,8 +86,16 @@ def gameLoop():
                     y1_change = snake_block
                     x1_change = 0
 
-        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-            game_close = True
+        # Проход змейки через стенки
+        if x1 >= dis_width:
+            x1 = 0
+        elif x1 < 0:
+            x1 = dis_width - snake_block
+        if y1 >= dis_height:
+            y1 = 0
+        elif y1 < 0:
+            y1 = dis_height - snake_block
+
         x1 += x1_change
         y1 += y1_change
         dis.fill(blue)
